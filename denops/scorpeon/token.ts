83f41e3ce772429ab19e9decd7803d6067cff2aa
uga-rosa <uga6603@gmail.com>
2022-11-14 01:16:45 +0900

    fix: type guard for json

--- denops/scorpeon/token.ts
+++ denops/scorpeon/token.ts
@@ -20,16 +20,38 @@ interface Language {
   extensions: string[];
 }
 
+const isLanguage = (obj: unknown): obj is Language => {
+  return obj !== null &&
+    typeof obj === "object" &&
+    "id" in obj &&
+    "extensions" in obj;
+};
+
 interface Grammar {
   language: string;
   scopeName: string;
   path: string;
 }
 
+const isGrammar = (obj: unknown): obj is Grammar => {
+  return obj !== null &&
+    typeof obj === "object" &&
+    "language" in obj &&
+    "scopeName" in obj &&
+    "path" in obj;
+};
+
 interface PackageJson {
-  contributes: {
-    languages: Language[];
-    grammars: Grammar[];
+  contributes?: {
+    languages?: {
+      id?: string;
+      extensions?: string[];
+    }[];
+    grammars?: {
+      language?: string;
+      scopeName?: string;
+      path?: string;
+    }[];
   };
 }
 
@@ -90,18 +112,18 @@ export class Tokenizer {
       for (const entry of expandGlobSync(`${dir}/*/package.json`)) {
         const text = Deno.readTextFileSync(entry.path);
         const jsonData: PackageJson = JSON.parse(text);
-        const _languages: Language[] | undefined = jsonData
+        const _languages = jsonData
           ?.contributes
           ?.languages
-          ?.filter((v) => v.id && v.extensions);
+          ?.filter(isLanguage);
         if (_languages) {
           languages = [...languages, ..._languages];
         }
-        const _grammars: Grammar[] | undefined = jsonData
+        const _grammars = jsonData
           ?.contributes
           ?.grammars
-          ?.filter((v) => v.language && v.scopeName && v.path)
-          ?.map((v) => {
+          ?.filter(isGrammar)
+          .map((v) => {
             v.path = join(entry.path, "..", v.path);
             return v;
           });

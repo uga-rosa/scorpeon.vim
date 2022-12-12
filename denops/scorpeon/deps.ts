94c3da35dc01014411303b4e0ef15694f1c7e6b3
uga-rosa <uga6603@gmail.com>
2022-12-13 02:15:16 +0900

    chore: update denops_std version

--- denops/scorpeon/deps.ts
+++ denops/scorpeon/deps.ts
@@ -2,13 +2,13 @@ export { default as vsctm } from "npm:vscode-textmate@7.0.3";
 export { default as oniguruma } from "npm:vscode-oniguruma@1.6.2";
 export { fromFileUrl, join } from "https://deno.land/std@0.161.0/path/mod.ts";
 export { expandGlobSync } from "https://deno.land/std@0.161.0/fs/mod.ts";
-export type { Denops } from "https://deno.land/x/denops_std@v3.9.1/mod.ts";
-export { g } from "https://deno.land/x/denops_std@v3.9.1/variable/variable.ts";
+export type { Denops } from "https://deno.land/x/denops_std@v3.11.0/mod.ts";
+export { g } from "https://deno.land/x/denops_std@v3.11.0/variable/variable.ts";
 export {
   batch,
   gather,
-} from "https://deno.land/x/denops_std@v3.9.1/batch/mod.ts";
-export { decorate } from "https://deno.land/x/denops_std@v3.9.1/buffer/mod.ts";
+} from "https://deno.land/x/denops_std@v3.11.0/batch/mod.ts";
+export { decorate } from "https://deno.land/x/denops_std@v3.11.0/buffer/mod.ts";
 export {
   assertArray,
   assertNumber,

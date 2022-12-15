7bc3659f1823c19df8b2e9c71c988a91f221a04b
uga-rosa <uga6603@gmail.com>
2022-12-15 16:29:18 +0900

    chore: update the version of denops_std

--- denops/scorpeon/deps.ts
+++ denops/scorpeon/deps.ts
@@ -1,18 +1,25 @@
 export { default as vsctm } from "npm:vscode-textmate@7.0.3";
 export { default as oniguruma } from "npm:vscode-oniguruma@1.6.2";
+
 export { fromFileUrl, join } from "https://deno.land/std@0.161.0/path/mod.ts";
 export { expandGlobSync } from "https://deno.land/std@0.161.0/fs/mod.ts";
-export type { Denops } from "https://deno.land/x/denops_std@v3.11.0/mod.ts";
-export { g } from "https://deno.land/x/denops_std@v3.11.0/variable/variable.ts";
+
+export type { Denops } from "https://deno.land/x/denops_std@v3.12.0/mod.ts";
+export { g } from "https://deno.land/x/denops_std@v3.12.0/variable/variable.ts";
 export {
   batch,
   gather,
-} from "https://deno.land/x/denops_std@v3.11.0/batch/mod.ts";
-export { decorate } from "https://deno.land/x/denops_std@v3.11.0/buffer/mod.ts";
+} from "https://deno.land/x/denops_std@v3.12.0/batch/mod.ts";
+export {
+  decorate,
+  undecorate,
+} from "https://deno.land/x/denops_std@v3.12.0/buffer/mod.ts";
+
 export {
   assertArray,
   assertNumber,
   assertObject,
   assertString,
 } from "https://deno.land/x/unknownutil@v2.0.0/mod.ts";
+
 export { default as cache_dir } from "https://deno.land/x/dir@1.5.1/cache_dir/mod.ts";
